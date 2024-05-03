3ff11c7155315213a62e5e23e85c547f0f37d732
naomi mali <derzsi_timea@yahoo.com>
2024-05-03 21:33:38 +0000

    add example of map method

--- map.js
+++ map.js
@@ -5,16 +5,28 @@
  */
 
 // Using a for loop
-
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results);
 
 // Using map()
+const multByTwo = function (num) {
+  return num * 2;
+}
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 
 // Simplified w/ map()
-
+const simplfied = nums.map(function (num) {return num * 2});
+console.log(simplfied);
 
 // Simplfied w/ map() + arrow function
-
+const arrow = nums.map(num => num * 2);
+console.log(arrow);
 
 // With objects:
 const students = [
@@ -22,18 +34,27 @@ const students = [
     id: 1,
     name: 'Mark',
     profession: 'Developer',
-    skill: 'JavaScript'
+    skill: 'JavaScript',
+    age: 32
   },
   {
     id: 2,
     name: 'Ariel',
     profession: 'Developer',
-    skill: 'HTML'
+    skill: 'HTML',
+    age: 23
   },
   {
     id: 3,
     name: 'Jason',
     profession: 'Designer',
-    skill: 'CSS'
+    skill: 'CSS',
+    age: 27
   },
 ];
+
+const studentsWithIds = students.map(student =>[student.name, student.id]);
+console.log(studentsWithIds);
+
+const studentsWithAge = students.map(student =>[student.name, student.age]);
+console.log(studentsWithAge);
